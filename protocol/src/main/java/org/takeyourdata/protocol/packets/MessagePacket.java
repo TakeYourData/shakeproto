d01be22f07f1ed94ad5128bb1d2f3f51d3eb0421
IdangorI <dangor.backup@petalmail.com>
2025-10-12 05:43:06 -0700

    encryptMessage and decryptMessage methods

--- protocol/src/main/java/org/takeyourdata/protocol/packets/MessagePacket.java
+++ protocol/src/main/java/org/takeyourdata/protocol/packets/MessagePacket.java
@@ -1,8 +1,13 @@
 package org.takeyourdata.protocol.packets;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Arrays;
 
 public class MessagePacket extends Packet {
     private String senderId;
@@ -19,11 +24,35 @@ public class MessagePacket extends Packet {
 
     public MessagePacket() {}
 
-//    public byte[] encryptMessage(byte[] content, byte[] sessionKey) throws Exception {
-//    }
-//
-//    public byte[] decryptMessage(byte[] sessionKey) throws Exception {
-//    }
+    public byte[] encryptMessage(byte[] content, byte[] sessionKey) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(sessionKey, "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        byte[] encrypted = cipher.doFinal(content);
+
+        byte[] result = new byte[iv.length + encrypted.length];
+        System.arraycopy(iv, 0, result, 0, iv.length);
+        System.arraycopy(encrypted, 0, result, iv.length, encrypted.length);
+
+        return result;
+    }
+
+    public byte[] decryptMessage(byte[] sessionKey) throws Exception {
+        byte[] iv = Arrays.copyOfRange(encryptedContent, 0, 16);
+        byte[] encrypted = Arrays.copyOfRange(encryptedContent, 16, encryptedContent.length);
+
+        SecretKeySpec keySpec = new SecretKeySpec(sessionKey, "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+        return cipher.doFinal(encrypted);
+    }
 
     @Override
     public void serializeData(DataOutputStream dos) throws IOException {
